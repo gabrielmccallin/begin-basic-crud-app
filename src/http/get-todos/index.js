0b45d400e02dc35211b77947279e6f0f49e7a6dc
Gabriel McCallin <gabriel.mccallin@outlook.com>
2021-01-14 13:31:04 +0000

    Return 200 instead of 201

--- src/http/get-todos/index.js
+++ src/http/get-todos/index.js
@@ -14,7 +14,7 @@ exports.handler = async function todos (req) {
   todos.sort((a, b) => a.created > b.created)
 
   return {
-    statusCode: 201,
+    statusCode: 200,
     headers: {
       'content-type': 'application/json; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
