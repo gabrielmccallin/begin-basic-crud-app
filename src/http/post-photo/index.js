cb12585c2fc32a347ea7d74a60a4a80fb8e9f157
Gabriel McCallin <gabriel.mccallin@hey.com>
2021-11-27 12:40:05 +0000

    ✨ return 400 for bad request

--- src/http/post-photo/index.js
+++ src/http/post-photo/index.js
@@ -2,7 +2,18 @@ let arc = require('@architect/functions')
 let data = require('@begin/data')
 
 exports.handler = async function post (req) {
-  let photo = arc.http.helpers.bodyParser(req) // Base64 decodes + parses body
+  try {
+    let photo = arc.http.helpers.bodyParser(req) // Base64 decodes + parses body
+  } catch {
+    return {
+      statusCode: 400,
+      headers: {
+        'content-type': 'application/json; charset=utf8',
+        'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+      },
+      body: "😢 Bad request"
+    }
+  }
   photo.created = photo.created || Date.now()
   await data.set({
     table: 'photos',
