e0007d0251bc09b5881b197ba28c52093e782696
Gabriel McCallin <gabriel.mccallin@hey.com>
2021-11-27 12:47:33 +0000

    ✨ how fast can we go?

---
+++ src/http/get-fast/index.js
@@ -0,0 +1,12 @@
+const data = require('@begin/data')
+
+exports.handler = async function fast (req) {
+  return {
+    statusCode: 200,
+    headers: {
+      'content-type': 'application/json; charset=utf8',
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+    },
+    body: "how fast? 🎽"
+  }
+}
