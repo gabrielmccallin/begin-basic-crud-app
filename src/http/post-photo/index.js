e7de1806a31f8b983045078a5413892d9b441776
Gabriel McCallin <gabriel.mccallin@hey.com>
2021-11-27 12:34:28 +0000

    ✨ return message for saved photo

--- src/http/post-photo/index.js
+++ src/http/post-photo/index.js
@@ -9,10 +9,11 @@ exports.handler = async function post (req) {
     ...photo
   })
   return {
-    statusCode: 302,
+    statusCode: 200,
     headers: {
-      location: '/',
+      'content-type': 'application/json; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-    }
+    },
+    body: "✊ Photo saved"
   }
 }
